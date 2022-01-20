fe44c7311409b2d9dedc9dcb7e4cbc2705ea5f81
rizwan-ak <rizwan.ak@outlook.com>
2022-01-21 00:09:28 +0100

    add pagination on API calls

--- src/common/api.js
+++ src/common/api.js
@@ -3,14 +3,13 @@ const api = axios.create({ baseURL: "https://swapi.dev/api/" });
 
 export const fetchPeople = async (pageNumber, people, callback) => {
   try {
-    let tempArray = [];
-    for (let idx = 1; idx <= 5; idx++) {
+    let tempArray = [...people];
+    for (let idx = 1 + (pageNumber - 1) * 5; idx <= 5 * pageNumber; idx++) {
       const response = await api.get(`people/${idx}/`);
       tempArray.push(response.data);
     }
     return callback(tempArray);
   } catch (err) {
-    // setIsError(true);
     console.error(err);
   }
 };
