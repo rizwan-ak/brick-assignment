c9d3bcd56618bf5f0b0291a499b33c2950c6440f
rizwan-ak <rizwan.ak@outlook.com>
2022-01-21 00:43:43 +0100

    add readme.md

--- src/common/components/List.js
+++ src/common/components/List.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { fetchPeople } from "../api";
 import BAButton from "../elements/BAButton";
 import BASearchField from "../elements/BASearchField";
-import ListItem from "./ListItem";
+import BATypography from "../elements/BATypography";
 import data from "../data.json";
 import { filterPeople } from "../utils";
+const ListItem = lazy(() => import("./ListItem"));
 
 export default function List() {
   const [pageNumber, setPageNumber] = useState(1);
@@ -32,15 +33,21 @@ export default function List() {
           idx
         ) => (
           <div key={idx}>
-            <ListItem
-              mass={mass}
-              name={name}
-              gender={gender}
-              skin_color={skin_color}
-              hair_color={hair_color}
-              eye_color={eye_color}
-              birth_year={birth_year}
-            />
+            <Suspense
+              fallback={
+                <BATypography value={data.ladingText} variant="loading" />
+              }
+            >
+              <ListItem
+                mass={mass}
+                name={name}
+                gender={gender}
+                skin_color={skin_color}
+                hair_color={hair_color}
+                eye_color={eye_color}
+                birth_year={birth_year}
+              />
+            </Suspense>
           </div>
         )
       )}
